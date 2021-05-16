2be9cfa74c7f8e56d1af305ed8b1d5376c09b869
Es180 <Es18.03.88>
2021-05-16 23:11:27 +0300

    Updates

---
+++ precache-manifest.9f256d2dd74c54c46d267118810ab9c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b4635becae80280b76781e18df03089",
+    "url": "/it-incubator-todolist-ts-19/index.html"
+  },
+  {
+    "revision": "aa0f7ec667aa6e680f05",
+    "url": "/it-incubator-todolist-ts-19/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "10335fa8e16e252a7a9a",
+    "url": "/it-incubator-todolist-ts-19/static/js/2.6e6559c3.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/it-incubator-todolist-ts-19/static/js/2.6e6559c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa0f7ec667aa6e680f05",
+    "url": "/it-incubator-todolist-ts-19/static/js/main.318a0625.chunk.js"
+  },
+  {
+    "revision": "a5a535041657c33ed70f",
+    "url": "/it-incubator-todolist-ts-19/static/js/runtime-main.b8c2e7b4.js"
+  }
+]);
